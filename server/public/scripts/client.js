14efee8cec1030c397a63267a14d184de7bf43df
Christopher Brenberg <cbrenberg@gmail.com>
2018-09-27 09:42:06 -0500

    require moment.js and use to format durations and dates for time entries

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -5,8 +5,9 @@ const app = angular.module('TimeTrackerApp', []);
 app.controller('EntriesController', ['$http', function ($http) {
   let vm = this;
   vm.entryToAdd = {};
+  vm.entries = []
 
-  vm.addEntry = function() {
+  vm.addEntry = function () {
     console.log('In addEntry');
     $http.post('/entries', {
       name: vm.entryToAdd.name,
@@ -15,11 +16,40 @@ app.controller('EntriesController', ['$http', function ($http) {
       start_time: vm.entryToAdd.start_time,
       end_time: vm.entryToAdd.start_time
     })
-    .then(function(response) {
-      console.log('Entry added', response);
-    })
-    .catch(function(error) {
-      console.log('Error adding entry', error);
-    })
-  }
-}])
+      .then(function (response) {
+        console.log('Entry added', response);
+      })
+      .catch(function (error) {
+        console.log('Error adding entry', error);
+      })//end $http
+  }//end addEntry
+
+  vm.getEntries = function () {
+    $http.get('/entries')
+      .then(function (response) {
+        console.log('Back from server with:', response);
+        // vm.entries = response.data;
+        //reformat duration as hours
+        // console.log(moment.duration(vm.entries[0].duration).asHours());
+        vm.entries = response.data.map(function(entry) {
+          entry.date = moment(entry.date).format("MM/DD/YYYY");
+          entry.duration = moment.duration(entry.duration).asHours();
+          if (entry.duration.toString().length > 3) {
+            entry.duration = entry.duration.toFixed(2);
+          } else {
+            entry.duration = entry.duration
+          }
+          return entry;
+        })//end map
+        console.log(vm.entries);
+        
+      })
+      .catch(function (error) {
+        console.log('Error getting entries:', error);
+      })//end $http
+  }//end getEntries
+
+  //call getEntries on page load
+  vm.getEntries();
+
+}])//end EntriesController
