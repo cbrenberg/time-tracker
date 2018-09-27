e1c871c89fa84f08d9060ced1a36982b21febfc2
Christopher Brenberg <cbrenberg@gmail.com>
2018-09-27 11:13:07 -0500

    reformat time inputs as 24hr HH:mm before POST

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,66 +1,21 @@
 console.log('client.js loaded');
 
-const app = angular.module('TimeTrackerApp', []);
+const app = angular.module('TimeTrackerApp', ['ngRoute']);
 
-app.controller('EntriesController', ['$http', function ($http) {
-  let vm = this;
-  vm.entryToAdd = {};
-  vm.entries = []
-
-  vm.addEntry = function () {
-    console.log('In addEntry');
-    $http.post('/entries', {
-      name: vm.entryToAdd.name,
-      project_id: vm.entryToAdd.project_id, //HOW TO SELECT ID? FROM ng-options???
-      date: vm.entryToAdd.date,
-      start_time: vm.entryToAdd.start_time, //HOW TO ENSURE PROPER FORMATTING FROM USER INPUT?
-      end_time: vm.entryToAdd.start_time //SEE ABOVE!
-    })
-      .then(function (response) {
-        console.log('Entry added', response);
-      })
-      .catch(function (error) {
-        console.log('Error adding entry', error);
-      })//end $http
-  }//end addEntry
-
-  vm.getEntries = function () {
-    $http.get('/entries')
-      .then(function (response) {
-        console.log('Back from server with:', response);
-        //reformat duration as hours rounded to two decimal places and date as MM/DD/YYYY
-        vm.entries = response.data.map(function(entry) {
-          entry.date = moment(entry.date).format("MM/DD/YYYY");
-          entry.duration = moment.duration(entry.duration).asHours();
-          if (entry.duration.toString().length > 3) {
-            entry.duration = entry.duration.toFixed(2);
-          }
-          //append reformatted object to vm.entries
-          return entry;
-        })//end map
-      })
-      .catch(function (error) {
-        console.log('Error getting entries:', error);
-      })//end $http
-  }//end getEntries
-
-  vm.deleteEntry = function(entryToDelete) {
-    console.log(entryToDelete)
-    $http.delete('/entries', 
+app.config(['$routeProvider', function ($routeProvider) {
+  $routeProvider
+    .when('/entries',
       {
-        params: {id: entryToDelete.id}
+        templateUrl: '../views/entries.html',
+        controller: 'EntriesController as vm'
       })
-      .then(function() {
-        console.log('Back from /entries DELETE: SUCCESS!');
-        //refresh table display
-        vm.getEntries();
+    .when('/projects',
+      {
+        templateUrl: '../views/projects.html',
+        controller: 'ProjectsController as vm'
       })
-      .catch(function(error) {
-        console.log('Error deleting item:', error);
-      })//end $http
-  }//end deleteEntry
+    .otherwise(
+      { template: `<h2>404 Not Found</h2>` }
+    );
+}]);
 
-  //call getEntries on page load
-  vm.getEntries();
-
-}])//end EntriesController
---
+++ server/public/scripts/controllers/entries.js
@@ -0,0 +1,63 @@
+// MODULE OUT INTO ENTRIES CONTROLLER
+app.controller('EntriesController', ['$http', function ($http) {
+  let vm = this;
+  vm.entryToAdd = {};
+  vm.entries = [];
+
+  vm.addEntry = function () {
+    console.log('In addEntry');
+    $http.post('/entries', {
+      name: vm.entryToAdd.name,
+      project_id: vm.entryToAdd.project_id, //HOW TO SELECT ID? FROM ng-options???
+      date: vm.entryToAdd.date,
+      start_time: moment(vm.entryToAdd.start_time, 'hh:mm A').format('HH:mm'), //HOW TO ENSURE PROPER FORMATTING FROM USER INPUT?
+      end_time: moment(vm.entryToAdd.end_time).format('HH:mm') //SEE ABOVE!
+    })
+      .then(function (response) {
+        console.log('Entry added', response);
+      })
+      .catch(function (error) {
+        console.log('Error adding entry', error);
+      })//end $http
+  }//end addEntry
+
+  vm.getEntries = function () {
+    $http.get('/entries')
+      .then(function (response) {
+        console.log('Back from server with:', response);
+        //reformat duration as hours rounded to two decimal places and date as MM/DD/YYYY
+        vm.entries = response.data.map(function (entry) {
+          entry.date = moment(entry.date).format("MM/DD/YYYY");
+          entry.duration = moment.duration(entry.duration).asHours();
+          if (entry.duration.toString().length > 3) {
+            entry.duration = entry.duration.toFixed(2);
+          }
+          //append reformatted object to vm.entries
+          return entry;
+        })//end map
+      })
+      .catch(function (error) {
+        console.log('Error getting entries:', error);
+      })//end $http
+  }//end getEntries
+
+  vm.deleteEntry = function (entryToDelete) {
+    console.log(entryToDelete)
+    $http.delete('/entries',
+      {
+        params: { id: entryToDelete.id }
+      })
+      .then(function () {
+        console.log('Back from /entries DELETE: SUCCESS!');
+        //refresh table display
+        vm.getEntries();
+      })
+      .catch(function (error) {
+        console.log('Error deleting item:', error);
+      })//end $http
+  }//end deleteEntry
+
+  //call getEntries on page load
+  vm.getEntries();
+
+}])//end EntriesController
