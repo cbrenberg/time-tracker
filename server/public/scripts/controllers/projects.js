2992cad6a9c22cbaaf2c6b0a1ba734e79b379f64
Christopher Brenberg <cbrenberg@gmail.com>
2018-09-27 11:55:01 -0500

    get project list to populate projects table

--- server/public/scripts/controllers/projects.js
+++ server/public/scripts/controllers/projects.js
@@ -1,19 +1,40 @@
-app.controller('ProjectsController', ['$http', function($http) {
+app.controller('ProjectsController', ['$http', function ($http) {
   let vm = this;
   vm.projectToAdd = {};
   vm.projects = [];
 
+
+  //add a new project
   vm.addProject = function () {
     console.log('In addProject');
     $http.post('/projects', {
       name: vm.projectToAdd.name
     })
-    .then(function() {
-      console.log('Project added');
-      // vm.getProjects();
-    })
-    .catch(function(err) {
-      console.log('Error adding project:', err);
-    })//end $http
+      .then(function () {
+        console.log('Project added');
+        // vm.getProjects();
+      })
+      .catch(function (err) {
+        console.log('Error adding project:', err);
+      })//end POST
   }//end addProject
+
+  //get projects
+  vm.getProjects = function () {
+    console.log('in getProjects');
+    $http.get('/projects')
+    .then(function(response) {
+      vm.projects = response.data;
+    })
+    .catch(function(error) {
+      console.log('Error getting project list', error);
+    })//end GET
+  }//end getProjects
+
+  //delete projects
+
+
+  //get project list on page load
+  vm.getProjects();
+
 }])//end Controller
